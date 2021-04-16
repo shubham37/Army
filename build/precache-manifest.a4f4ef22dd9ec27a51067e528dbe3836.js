36422dd67b1a4283d4e4906c99f525523c2ce0de
shubham <sps371997@gmail.com>
2021-04-17 00:38:14 +0530

    add build

---
+++ build/precache-manifest.a4f4ef22dd9ec27a51067e528dbe3836.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e77803f43985ba6b264e76a4f074969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981927cf9ad4b67a9a2d",
+    "url": "/static/css/3.bdf2e429.chunk.css"
+  },
+  {
+    "revision": "0447690f56c64e091388",
+    "url": "/static/css/main.0f489aaa.chunk.css"
+  },
+  {
+    "revision": "1093c8203a2f50d2960b",
+    "url": "/static/js/2.a684b7a3.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.a684b7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981927cf9ad4b67a9a2d",
+    "url": "/static/js/3.2384dc7a.chunk.js"
+  },
+  {
+    "revision": "92fab4879e27b6e95ec0087919af8e08",
+    "url": "/static/js/3.2384dc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3609d5be237a288fa4",
+    "url": "/static/js/4.4bfb2d41.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.4bfb2d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52559499b6f5ec616ab",
+    "url": "/static/js/5.a8dd42c3.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.a8dd42c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0447690f56c64e091388",
+    "url": "/static/js/main.15872e20.chunk.js"
+  },
+  {
+    "revision": "06f1810e25f45c28aedf",
+    "url": "/static/js/runtime-main.9672fef0.js"
+  },
+  {
+    "revision": "f4581a3e91836957856977cd5ec2e1fc",
+    "url": "/static/media/airforce.f4581a3e.jpeg"
+  },
+  {
+    "revision": "8924a2c2c21babe5807a10f524ee89fd",
+    "url": "/static/media/bot.8924a2c2.jpg"
+  },
+  {
+    "revision": "ff7f47f6372e853c5bf278c9e08a5b13",
+    "url": "/static/media/cancel.ff7f47f6.svg"
+  },
+  {
+    "revision": "ccc2b25ef1999e8c9bd7bd1370fabb8e",
+    "url": "/static/media/coast_guard.ccc2b25e.png"
+  },
+  {
+    "revision": "3eba558833396771539191c6cca08739",
+    "url": "/static/media/indianarmy.3eba5588.jpg"
+  },
+  {
+    "revision": "07f95f28c56e2d776468e6997e2d3739",
+    "url": "/static/media/meter.07f95f28.jpg"
+  },
+  {
+    "revision": "4935b4e9b9be84b5adba7c92502c8c28",
+    "url": "/static/media/navy.4935b4e9.jpg"
+  },
+  {
+    "revision": "69a0cf43c34f1c53b5cfb5caba731f3c",
+    "url": "/static/media/ssb_logo.69a0cf43.png"
+  },
+  {
+    "revision": "fdb75b147a27e74a798c06e676a957c3",
+    "url": "/static/media/wa.fdb75b14.png"
+  }
+]);
