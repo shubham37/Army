3f8482e7379a0f22a273138ada40c23630ca57d8
shubham <sps371997@gmail.com>
2021-04-17 11:11:01 +0530

    recheck

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,28 +4,28 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://54.69.172.40:8000',
       changeOrigin: true,
     })
   );
   app.use(
     '/student_api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://54.69.172.40:8000',
       changeOrigin: true,
     })
   );  
   app.use(
     '/assessor_api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://54.69.172.40:8000',
       changeOrigin: true,
     })
   );  
   app.use(
     '/admin_api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://54.69.172.40:8000',
       changeOrigin: true,
     })
   );
